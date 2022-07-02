9c87ee4560d77c00c86bde1305e427e5b666b382
ciaralynch72 <Ciaralynch72@gmail.com>
2022-07-02 16:57:42 +0000

    Add progression counters to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,50 +2,50 @@ const questionHolder = document.getElementById('question')
 const possibleAnswers = document.getElementById('multiChoice')
 const startButton = document.getElementById('startQuiz')
 const nextQuestion = document.getElementById('nextQuestion')
-const welcomeMessage = document.getElementsByClassName('welcomeMessage')
-const hidden = document.getElementsByClassName('hidden')
+// const welcomeMessage = document.getElementsByClassName('welcomeMessage')
+// const hidden = document.getElementsByClassName('hidden')
 
 //Quiz questions 
 const quizQuestions = [{
     question: "Which was Canada's first official National Park?",
     choices: ['Banff', 'Glacier', 'Qausuittuq', 'Elk Island'],
-    correctAnswer: 1
+    correcAnswer: 0
 },
 {
     question: 'What is a toque?',
     choices: ['Tool to stoke a fire', 'Traditional instrument', 'Wooly hat', 'A bird'],
-    correctAnswer: 3
+    correctAnswer: 2
 },
 {
     question: "What is Canada's only official bilingual province?",
     choices: ['Quebec', 'New Brunswick', 'Manitoba', 'Nova Scotia'],
-    correctAnswer: 2
+    correctAnswer: 1
 },
 {
     question: 'What is the name of the deepest lake in Canada?',
     choices: ['Lake Louise', 'Morraine Lake', 'Great Slave Lake', '	Lac Mistassini'],
-    correctAnswer: 3
+    correctAnswer: 2
 },
 {
     question: 'How many times zones does Canada have?',
     choices: ['3', '6', '4', '5'],
-    correctAnswer: 2 
+    correctAnswer: 1 
 },
 {
     question: 'What animal is an offical emblem of Canada?',
     choices: ['Moose', 'Bear', 'Beaver', 'Racoon'],
-    correctAnswer: 3
+    correctAnswer: 2
 },
 {
     question: 'Which of the following was NOT a Canadian Invention?',
     choices: ['The Paint Roller', 'Peanut Butter', 'Electric Wheelchair', 'Chainsaw'],
-    correctAnswer: 4
+    correctAnswer: 3
 }]
 
 
 //Initiating some variables
 let i = 0;
-const correctAnswer = 0;
+const currentquestion = 0;
 
 nextQuestion.style.display = 'none';
 
@@ -59,15 +59,14 @@ nextQuestion.style.display = 'none';
 
 
 startButton.onclick = function() {
-    /*itterate through questions*/  
+    /*iterate through all the questions*/  
     if(i > quizQuestions.length -1){
        i=0;       
     }    
     displayQuestion(i);
     i++;
     startButton.innerHTML = "Next";
-    displayQuestion();
-    console.log('do you do anything')
+    console.log('Start Function')
 }
 
 //displayQuestion
@@ -81,6 +80,7 @@ function displayQuestion(qNum) {
         let choiceText = individualQuestion.choices[key];
         possibleAnswers.appendChild(createLi(radioBtnName,choiceText));
     }
+    console.log('display function ')
 }
 //create list of possible answers 
 function createLi(name, choiceText) {
@@ -89,6 +89,12 @@ function createLi(name, choiceText) {
     radioHtml += '/>';
     radioHtml += choiceText;        
     list.innerHTML = radioHtml;
+    console.log('creat list items')
     return list;
+}
+
+function questionCounter() {
+    let counter = parseInt(document.getElementById("questionCounter").innerText);
+    document.getElementById("questionCounter").innerText = ++counter;
 }
 
